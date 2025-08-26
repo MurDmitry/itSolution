7555f1d1906830004df29eb4de67a1de83a5da45
Murzaev Dmitry <d.murzaev@pzm.su>
2025-08-26 15:19:23 +0300

    Сделал папку templates. Создал html файлы для будущих страниц

--- quotes_app/views.py
+++ quotes_app/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Домашняя старница
 def home_page(request):
-    return HttpResponse("<h1>Домашняя страница</h1>")
+    return render(request, 'home.html')
 
 # Страница с 10 самыми популярными цитатами
 def the_best_quotes(request):
-    return HttpResponse("<h1>Топ 10 цитат</h1>")
+    return render(request, 'best_quotes.html')
