1b67f588c7a2f969fa4c1eb0618f0af7d5c285a5
Murzaev Dmitry <d.murzaev@pzm.su>
2025-08-26 15:40:44 +0300

    Настройка расположения файлов. Добавил тестовые (статичные) данные для проверки отображения таблицы (пока без стилей) для страницы "Топ 10 цитат"

--- quotes_app/urls.py
+++ quotes_app/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.home_page, name="home"),                           # Домашняя страница
-    path('top_quotes', views.the_best_quotes, name="the_best_quotes") # Топ 10 цитат по лайкам
+    path('', views.home_page, name="home"),                            # Домашняя страница (home.html)
+    path('best_quotes', views.the_best_quotes, name="the_best_quotes") # Топ 10 цитат по лайкам (best_quotes.html)
 ]
--- quotes_app/views.py
+++ quotes_app/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 
+# Тестовые цитаты для 10 лучших 
+quotes = [
+    {'id': 1, 'quote': 'Цитата 1', 'rating': 7},
+    {'id': 2, 'quote': 'Цитата 2', 'rating': 2},
+    {'id': 3, 'quote': 'Цитата 3', 'rating': 10},
+    {'id': 4, 'quote': 'Цитата 4', 'rating': 8},
+]
+
 # Домашняя старница
 def home_page(request):
-    return render(request, 'home.html')
+    return render(request, 'base/home.html')
 
 # Страница с 10 самыми популярными цитатами
 def the_best_quotes(request):
-    return render(request, 'best_quotes.html')
+    context = {'quotes': quotes}
+    return render(request, 'base/best_quotes.html', context)
