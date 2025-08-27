0acee18a0063f4dfaac3eef70987899c214349c6
Murzaev Dmitry <d.murzaev@pzm.su>
2025-08-27 19:52:44 +0300

    Добавил к цитате информацию откуда она, автора и год. Добавил тестовые данные в БД

--- quotes_app/views.py
+++ quotes_app/views.py
@@ -5,17 +5,24 @@ import random
 
 # Домашняя старница
 def home_page(request):
-    quotes = list(Quote.objects.all())   # Данные из таблицы Quote
+    quotes = list(Quote.objects.all().select_related('source'))   # Данные из таблицы Quote и Source
 
     if not quotes:
         quote_text = "Цитаты не найдены"
+        context = {'quote_text': quote_text}        
     else:
         # Взвешенный случайный выбор (использую random.choices с указанием весов)
         selected_quote = random.choices(quotes, weights=[quote.weight for quote in quotes], k=1)[0]
-        quote_text = selected_quote.text
 
-    content = {'quote_text': quote_text}
-    return render(request, 'base/home.html', content)
+        context = {
+            'quote_text': selected_quote.text,                 # Текст цитаты
+            'quote_author': selected_quote.author,             # Автор цитаты
+            'source_title': selected_quote.source.title,       # Откуда (фильм/книга/игра и так далее)
+            'source_author': selected_quote.source.author,     # Автор произведения
+            'source_year': selected_quote.source.year          # Год   
+        }
+   
+    return render(request, 'base/home.html', context)
 
 
 # Страница с 10 (на данный момент со всеми. Пока нет фильтрации) самыми популярными цитатами
