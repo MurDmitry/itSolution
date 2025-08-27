9f6b8e85465712dd614a6ab55f66786a712f572a
Murzaev Dmitry <d.murzaev@pzm.su>
2025-08-27 18:58:20 +0300

    Добавил оформление цитаты на странице и взвешенный выбор (чем больше вес - тем вероятнее попадется) одной цитаты

--- quotes_app/views.py
+++ quotes_app/views.py
@@ -1,12 +1,24 @@
 from django.shortcuts import render
 from .models import Quote, Source
+import random
 
 
 # Домашняя старница
 def home_page(request):
-    return render(request, 'base/home.html')
+    quotes = list(Quote.objects.all())   # Данные из таблицы Quote
 
-# Страница с 10 самыми популярными цитатами
+    if not quotes:
+        quote_text = "Цитаты не найдены"
+    else:
+        # Взвешенный случайный выбор (использую random.choices с указанием весов)
+        selected_quote = random.choices(quotes, weights=[quote.weight for quote in quotes], k=1)[0]
+        quote_text = selected_quote.text
+
+    content = {'quote_text': quote_text}
+    return render(request, 'base/home.html', content)
+
+
+# Страница с 10 (на данный момент со всеми. Пока нет фильтрации) самыми популярными цитатами
 def the_best_quotes(request):
     quotes = Quote.objects.all()    # Данные из таблицы Quote
     context = {'quotes': quotes}
