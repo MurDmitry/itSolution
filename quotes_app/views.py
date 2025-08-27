8a87df9c86df51fc4f77271013365222bfd624a3
Murzaev Dmitry <d.murzaev@pzm.su>
2025-08-27 16:35:57 +0300

    Добавил тестовые данные, получаю теперь их из таблицы в БД. Добавил стили из Bootstrap, немного поработал над оформлением. Добавил шапку страницы

--- quotes_app/views.py
+++ quotes_app/views.py
@@ -1,12 +1,6 @@
 from django.shortcuts import render
+from .models import Quote, Source
 
-# Тестовые цитаты для 10 лучших 
-quotes = [
-    {'id': 1, 'quote': 'Цитата 1', 'rating': 7},
-    {'id': 2, 'quote': 'Цитата 2', 'rating': 2},
-    {'id': 3, 'quote': 'Цитата 3', 'rating': 10},
-    {'id': 4, 'quote': 'Цитата 4', 'rating': 8},
-]
 
 # Домашняя старница
 def home_page(request):
@@ -14,5 +8,6 @@ def home_page(request):
 
 # Страница с 10 самыми популярными цитатами
 def the_best_quotes(request):
+    quotes = Quote.objects.all()    # Данные из таблицы Quote
     context = {'quotes': quotes}
     return render(request, 'base/best_quotes.html', context)
